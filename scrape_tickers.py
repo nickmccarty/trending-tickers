5215b822318d1f1c700a03230c447139523d406a
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2024-05-09 18:44:56 -0700

    reverting

--- scrape_tickers.py
+++ scrape_tickers.py
@@ -58,8 +58,8 @@ def scrape_trending_tickers():
     company_names = trending_tickers_df['Name'].tolist()
     last_price = trending_tickers_df['Last Price'].tolist()
     percent_changes = trending_tickers_df['% Change'].astype(str).str.lstrip('+').str.replace(',', '').str.rstrip('%').astype(float).tolist()
-    trading_volume = trending_tickers_df['Volume'].astype(str).str.strip().tolist()
-    market_cap = trending_tickers_df['Market Cap'].astype(str).str.strip().tolist()
+    trading_volume = trending_tickers_df['Volume'].str.strip().tolist()
+    market_cap = trending_tickers_df['Market Cap'].str.strip().tolist()
 
     sector = []
     industry = []
