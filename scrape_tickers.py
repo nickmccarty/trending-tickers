ee378ee9fcf5ec2a404f4ad5206c26b716b3e829
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2025-02-20 14:13:00 -0800

    changing URL to scrape info for <= 200 most active tickers

--- scrape_tickers.py
+++ scrape_tickers.py
@@ -33,7 +33,8 @@ def get_recent_news(ticker):
 
 def scrape_trending_tickers():
     current_time = datetime.now()
-    url = "https://finance.yahoo.com/markets/stocks/trending/"
+    # url = "https://finance.yahoo.com/markets/stocks/trending/"
+    url = "https://finance.yahoo.com/markets/stocks/most-active/?start=0&count=200"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
